48adc95acfebcdd1c5351c11ad57711764bc9938
veranyc <veranyc@gmail.com>
2017-05-29 14:13:32 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_accessor :backed_projects, :name, :project, :backers
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+
+
+
+end
